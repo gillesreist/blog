a434e1e1f127452f5089a50a9a02699e9429694e
Damabiah <gilles.reist@gmail.com>
2023-06-29 10:11:43 +0200

    création du frontcontroller et de l'architecture

--- index.php
+++ index.php
@@ -1,1 +1,25 @@
-<?= 'Bienvenue sur le blog' ?>
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+session_start();
+
+$action = filter_input(INPUT_GET, "action", FILTER_SANITIZE_URL);
+
+$route = [
+    "" => "index.php"
+];
+
+$render="";
+
+ob_start();
+
+if (!empty($route[$action])) {
+    include $route[$action];
+} else {
+    include "404.php";
+}
+
+$render = ob_get_clean();
+
+echo "$render";
